c6a13e0a353dd7db9043112165596f4d4e485779
P0159775 <xinhao.yang@pactera.com>
2020-11-26 22:55:53 +0800

    doc

---
+++ docs/components/index.md
@@ -0,0 +1,7 @@
+---
+id: index
+title: 简介
+sidebar_label: 介绍
+---
+
+开发中常用的组件库
---
+++ sidebars.js
@@ -0,0 +1,17 @@
+module.exports = {
+  someSidebar: {
+    "规范文档": ['regulation/code', 'regulation/git'],
+    // Features: ['mdx'],
+  },
+  com:{
+    "组件": ['components/index','components/message'],
+  },
+
+  vscode:{
+    "vscode": ['tools/vscode/vscode','tools/vscode/shortcutkey'],
+  },
+
+  sourcetree:{
+    "sourcetree": ['tools/sourcetree/sourcetree'],
+  },
+};
